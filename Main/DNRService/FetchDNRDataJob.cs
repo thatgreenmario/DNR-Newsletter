777fc8757f6d08cbaa93b0cf9c49128acade04a8
thatgreenmario <oj1696@gmail.com>
2023-09-23 15:41:04 +0530

    Add project files.

---
+++ Main/DNRService/FetchDNRDataJob.cs
@@ -0,0 +1,13 @@
+﻿using Quartz;
+
+namespace Main.DNRDataFetcher
+{
+    public class FetchDNRDataJob : IJob
+    {
+        public Task Execute(IJobExecutionContext context)
+        {
+            Console.WriteLine("Hello, Quartz.NET!");
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Main/Program.cs
@@ -0,0 +1,38 @@
+using Main.DNRDataFetcher;
+using Main.Quartz;
+using Quartz;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//Implement Quartz Scheduler
+builder.Services.AddQuartz(q =>
+{
+    // Register the job, loading the schedule from configuration
+    q.AddJobAndTrigger<FetchDNRDataJob>(builder.Configuration);
+});
+
+builder.Services.AddQuartzHostedService(q => q.WaitForJobsToComplete = true);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
